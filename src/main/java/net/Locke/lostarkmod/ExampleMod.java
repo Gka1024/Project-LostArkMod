90f9e6b65579d43f68c0de14ad7305108ecf35bb
Gka1024 <rhrhtlddkswn@naver.com>
2024-08-31 16:35:07 +0900

    test commit

--- src/main/java/net/Locke/lostarkmod/ExampleMod.java
+++ src/main/java/net/Locke/lostarkmod/ExampleMod.java
@@ -36,7 +36,7 @@ public class ExampleMod
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "lostarkmod";
     // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
     
     public ExampleMod()
     {
