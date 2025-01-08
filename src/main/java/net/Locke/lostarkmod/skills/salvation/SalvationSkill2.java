7944df05082b8770a42835384d81b9ac68337b78
Gka1024 <ththtlddkswn@gmail.com>
2025-01-09 03:35:51 +0900

    Adding Salvation Skill (complete without Mana)

--- src/main/java/net/Locke/lostarkmod/skills/salvation/SalvationSkill2.java
+++ src/main/java/net/Locke/lostarkmod/skills/salvation/SalvationSkill2.java
@@ -1,9 +1,13 @@
 package net.Locke.lostarkmod.skills.salvation;
 
+import net.Locke.lostarkmod.capability.IMana;
+import net.Locke.lostarkmod.capability.Mana;
+import net.Locke.lostarkmod.capability.ManaProvider;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.projectile.Arrow;
 import net.minecraft.world.item.BowItem;
 import net.minecraft.world.item.CrossbowItem;
@@ -17,65 +21,113 @@ public class SalvationSkill2 {
 
     static int SKILL_BOW_DELAY_TICKS = 60;
     static long LAST_BOW_SKILL_USED_TIME = 0;
+    static long LAST_CROSSBOW_SKILL_USED_TIME = 0;
     static long CHARGE_START_TIME = 0;
-    static Boolean isSkillAvailable = false;
 
-    public static void tick()
-    {
-        //checkBowSkillCooltime();
-        if(isSkillHolding)
-        {
-            showCurrentKeyDown(getCurrentGameTime() - CHARGE_START_TIME, 30 , 40);
+    static long BOW_SKILL_COOLTIME = 60; // 3sec
+    static long CROSSBOW_SKILL_COOLTIME = 200; // 10sec
+    static Boolean isBowSkillAvailable = true;
+    static Boolean isCrossbowSkillAvailable = true;
+
+    public static void tick() {
+        checkBowSkillCooltime();
+        checkCrossbowSkillCooltime();
+
+        if (isSkillHolding && isCrossbowSkillAvailable) {
+            showCurrentKeyDown(getCurrentGameTime() - CHARGE_START_TIME, 30, 40);
         }
     }
 
-    private static long getCurrentGameTime()
-    {
-        if(Minecraft.getInstance().level != null)
-        {
+    private static long getCurrentGameTime() {
+        if (Minecraft.getInstance().level != null) {
             return Minecraft.getInstance().level.getGameTime();
         }
         return 0;
     }
 
-
     public static void skillUse(Player player) {
         ItemStack item = player.getMainHandItem();
         if (item.getItem() instanceof BowItem) {
             bowSkillUse(player);
+        } else if (item.getItem() instanceof CrossbowItem) {
+            if (!isSkillHolding) {
+                isSkillHolding = true;
+                CHARGE_START_TIME = getCurrentGameTime();
+            }
         }
-        else if(item.getItem() instanceof CrossbowItem)
-        {
-            crossbowSkillCharge();
-
-            isSkillHolding = true;
-            CHARGE_START_TIME = getCurrentGameTime();
-        }
-        
     }
 
     public static void skillUse(Player player, int chargeTime) {
         ItemStack item = player.getMainHandItem();
         if (item.getItem() instanceof CrossbowItem) {
-            crossBowSkillUse(chargeTime);
+            crossBowSkillUse(player, chargeTime);
             isSkillHolding = false;
         }
     }
 
     private static void bowSkillUse(Player player) {
-
-        if(isSkillAvailable)
-        {
+        if (isBowSkillAvailable) {
             playerKnockBack(player);
             createArrow(player, 2.0f);
-            isSkillAvailable = false;
-
+            LAST_BOW_SKILL_USED_TIME = getCurrentGameTime();
+            isBowSkillAvailable = false;
+            useMana(player, 20);
         }
-        
+    }
+
+    private static void checkBowSkillCooltime() {
+        if (LAST_BOW_SKILL_USED_TIME + BOW_SKILL_COOLTIME < getCurrentGameTime()) {
+            isBowSkillAvailable = true;
+        }
+    }
+
+    private static void checkCrossbowSkillCooltime() {
+        if (LAST_CROSSBOW_SKILL_USED_TIME + CROSSBOW_SKILL_COOLTIME < getCurrentGameTime()) {
+            isCrossbowSkillAvailable = true;
+        }
     }
 
     private static Vec3 getLookVector(Player player) {
         return player.getLookAngle().normalize();
+    }
+
+    private static void crossBowSkillUse(Player player, int time) {
+
+        if (isCrossbowSkillAvailable) {
+            LAST_CROSSBOW_SKILL_USED_TIME = getCurrentGameTime();
+            isCrossbowSkillAvailable = false;
+            useMana(player, 40);
+            if (time > 30 && time < 40) {
+                playerKnockBack(player);
+                createArrow(player, 20f);
+            } else {
+                createArrow(player, 2.0f);
+            }
+        }
+    }
+
+    private static void showCurrentKeyDown(long timeTicks, int startTick, int endTick) // 50
+    {
+        LocalPlayer player = Minecraft.getInstance().player;
+
+        if (player == null) {
+            return;
+        }
+
+        String displayString = "";
+
+        for (int i = 0; i < 15; i++) {
+            if (timeTicks > (i * 10) / 3) {
+                displayString += "=";
+            } else {
+                displayString += "-";
+            }
+            if (i == (startTick * 3) / 10 || i == (endTick * 3) / 10) {
+                displayString += "|";
+            }
+        }
+
+        player.displayClientMessage(Component.literal(displayString), true);
     }
 
     private static void playerKnockBack(Player player) {
@@ -89,49 +141,30 @@ public class SalvationSkill2 {
     private static void createArrow(Player player, float ArrowSpeed) {
         Level world = player.level();
         Arrow arrow = new Arrow(world, player);
-
-        Vec3 lookVector = getLookVector(player);
-        arrow.setDeltaMovement(lookVector.scale(ArrowSpeed));
+        /*
+         * Vec3 lookVector = getLookVector(player);
+         * Vec3 arrowVector = lookVector.normalize();
+         * arrow.setDeltaMovement(arrowVector.scale(ArrowSpeed));
+         */
+        arrow.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, ArrowSpeed, 0F);
+        // System.out.println(lookVector);
+        // 화살 데미지는 root(speed ^2) * 2
         arrow.setOwner(player);
-
+        arrow.pickup = AbstractArrow.Pickup.CREATIVE_ONLY;
         world.addFreshEntity(arrow);
     }
 
-    private static void crossBowSkillUse(int time)
-    {
-        
-        
+    private static void useMana(Player player, int mana) {
+        IMana playerMana = player.getCapability(ManaProvider.MANA_CAPABILITY).orElse(new Mana());
+        playerMana.useMana(mana);
     }
 
-    private static void crossbowSkillCharge()
-    {
-        long curTime = Minecraft.getInstance().level.getGameTime();
-
-    }
-
-    private static void showCurrentKeyDown(long timeTicks, int startTick, int endTick) // 50
-    {
-        LocalPlayer player = Minecraft.getInstance().player;
-
-        if(player == null)
-        {
-            return;
+    private static boolean checkPlayerMana(Player player, int mana) {
+        IMana playerMana = player.getCapability(ManaProvider.MANA_CAPABILITY).orElse(new Mana());
+        if (playerMana.getMana() > mana) {
+            return true;
         }
-
-        String displayString = "";
-
-        for (int i = 0; i < 15; i++) {
-            if (timeTicks > (i * 10) / 3) {
-                displayString += "=";
-            } else {
-                displayString += "-";
-            }
-            if (i == (startTick * 3) / 10 || i == (startTick * 3) / 10) {
-                displayString += "|";
-            }
-        }
-
-        player.displayClientMessage(Component.literal(displayString), true);
+        return false;
     }
-    
+
 }
